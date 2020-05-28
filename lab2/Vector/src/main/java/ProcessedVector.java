e426fd584995290b26e1d9493bb125728c241c9b
OVERENSAL <mle8@yandex.ru>
2020-05-28 20:56:46 +0300

    fixed subtract on zero

--- lab2/Vector/src/main/java/ProcessedVector.java
+++ lab2/Vector/src/main/java/ProcessedVector.java
@@ -5,9 +5,13 @@ public class ProcessedVector {
     public Vector<Double> getProcessedVector(Vector<Double> vector) {
         double maxElement = Collections.max(vector);
         for (int i = 0; i < vector.size(); i++) {
-            double number = vector.get(i) / (maxElement / 2);
-            double roundedNumber = Math.round(number * 1000) / 1000.0;
-            vector.set(i, roundedNumber);
+            try {
+                double number = vector.get(i) / (maxElement / 2);
+                double roundedNumber = Math.round(number * 1000) / 1000.0;
+                vector.set(i, roundedNumber);
+            } catch (NullPointerException e) {
+                System.out.println(e.getLocalizedMessage());
+            }
         }
 
         return vector;
